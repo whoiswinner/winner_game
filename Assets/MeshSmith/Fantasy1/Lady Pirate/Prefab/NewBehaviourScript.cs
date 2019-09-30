bd33bad7c73023fd42d158a064ccca4526e5578c
이현수 <dlgustn675@naver.com>
2019-09-30 16:19:53 +0900

    맵 디자인
    게임에 사용할 맵

---
+++ Assets/MeshSmith/Fantasy1/Lady Pirate/Prefab/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    float speed = 10f;
+
+    Rigidbody rigidbody;
+    Vector3 movement;
+    float h, v;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+    void FixedUpdate()
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rigidbody.MovePosition(transform.position + movement);
+    }
+
+}
