6405fe1138d6e218041477e9d129101267329ec7
이현수 <dlgustn675@naver.com>
2019-10-20 19:36:05 +0900

    캐릭터 회전 코드 수 정해

--- Assets/MeshSmith/Fantasy1/Player/Scripts/Playercontroller.cs
+++ Assets/MeshSmith/Fantasy1/Player/Scripts/Playercontroller.cs
@@ -5,47 +5,80 @@ using UnityEngine;
 public class Playercontroller : MonoBehaviour
 {
 
-    public float speed;      // 캐릭터 움직임 스피드.
-    public float jumpSpeed; // 캐릭터 점프 힘.
-    public float gravity;    // 캐릭터에게 작용하는 중력.
+    public float speed = 2f;
+    public float jumpPower = 5f;
+    public float rotateSpeed = 4f;
 
-    private CharacterController controller; // 현재 캐릭터가 가지고있는 캐릭터 컨트롤러 콜라이더.
-    private Vector3 MoveDir;                // 캐릭터의 움직이는 방향.
+    float horizontalMove;
+    float verticalMove;
 
-    void Start()
+    Rigdbody rigdbody;
+    Animator animator;
+
+    Vector3 movenment;
+    bool IsJumping;
+
+    void Awake()
     {
-        speed = 6.0f;
-        jumpSpeed = 8.0f;
-        gravity = 20.0f;
-
-        MoveDir = Vector3.zero;
-        controller = GetComponent<CharacterController>();
+        rigdbody = GetComponent<Rigdbody>();
+        animator = GetComponent<Animator>();
     }
 
-    void Update()
+    void Updata()
     {
-        // 현재 캐릭터가 땅에 있는가?
-        if (controller.isGrounded)
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        verticalMove = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Jump"))
+            IsJumping = true;
+
+        AnimationUpdate();
+    }
+
+    void FixedUpdate()
+    {
+        jumpPower();
+        Run();
+        Turn();
+    }
+
+    void AnimationUpdata()
+    {
+        if (horizontalMove == 0 && verticalMove == 0)
         {
-            // 위, 아래 움직임 셋팅. 
-            MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-            // 벡터를 로컬 좌표계 기준에서 월드 좌표계 기준으로 변환한다.
-            MoveDir = transform.TransformDirection(MoveDir);
-
-            // 스피드 증가.
-            MoveDir *= speed;
-
-            // 캐릭터 점프
-            if (Input.GetButton("Jump"))
-                MoveDir.y = jumpSpeed;
-
+            animator.SetBool("IsRunning", false);
         }
-
-        // 캐릭터에 중력 적용.
-        MoveDir.y -= gravity * Time.deltaTime;
-
-        // 캐릭터 움직임.
-        controller.Move(MoveDir * Time.deltaTime);
+        else
+            animator.SetBool("IsRunning", true);
     }
-}
+    void Run()
+    {
+        movement.Set(horizontalMove, 0, verticalMove);
+        movenment = movenment.normalized * speed * Time.deltaTime;
+        rigdbody.MovePosition(transform.position + movenment);
+    }
+
+    void Turn()
+    {
+        if (horizontalMove == 0 && verticalMove == 0)
+            return;
+
+        Quaternion newRotation = Quaternion.LookRotation(movement);
+
+        rigdbody.rotation = Quaternion.Slerp(rigdbody.rotation, newRotation, rotateSpeed * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        if (!IsJumping)
+            return;
+
+        rigdbody.AddForce(transform.up * jumpPower, ForceMode.Impulse);
+
+        IsJumping = false;
+    }
+
+    void start()
+    {
+
+    }
