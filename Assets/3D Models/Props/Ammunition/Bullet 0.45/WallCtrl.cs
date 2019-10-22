c642cc012c485a455b1746015f5d67b7d15c30cc
이현수 <dlgustn675@naver.com>
2019-10-23 05:11:43 +0900

    fire add

---
+++ Assets/3D Models/Props/Ammunition/Bullet 0.45/WallCtrl.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCtrl : MonoBehaviour
+{
+    void OnCallisionEnter(Collision col)
+    {
+        if (col.collider.tag == "Bullet") Destroy(col.gameObject);
+    }
+}
