e8a64d4c250b110d963fa5cd23a3fab4b5b917f0
이현수 <dlgustn675@naver.com>
2019-10-30 05:59:00 +0900

    스킬 +1
    스킬 한개 더 구현

---
+++ Assets/3D Models/Props/Ammunition/Bullet 0.45/FastBullet.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FastBullet : MonoBehaviour
+{
+    public int Damage = 20;
+    public float Speed = 1000.0f;
+    public Rigidbody rigidbody;
+
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        Fastbulletshot();
+    }
+
+
+
+    void Fastbulletshot()
+    {
+        rigidbody.AddForce(transform.up * Speed);
+    }
+}
---
+++ Assets/Sun_Temple/Scripts/touch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvent : MonoBehaviour
+{
+    public GameObject button;
+
+    public void changeMaterial()
+    {
+        gameObject.GetComponent<Renderer>().material.color = Color.blue;
+
+    }
+}
