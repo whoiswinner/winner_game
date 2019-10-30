2a9086ba4769a77cf49c52d0db5a50e93a2b58e6
김도균 <shellcodesniper@gmail.com>
2019-10-31 05:12:28 +0900

    Bullet Controller

---
+++ Assets/MeshSmith/Fantasy1/Player/Scripts/Bullet_Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+
+        Debug.Log("Remove GameObject Self");
+
+
+        Destroy(gameObject);
+    }
+}
